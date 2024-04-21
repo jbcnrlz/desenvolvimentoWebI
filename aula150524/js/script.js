1110222aeb3e1bc202db1e059743f5b7388010ab
João Baptista Carida Neto <jbcnrlz@gmail.com>
2024-04-21 19:56:34 -0300

    Introdução Javascript

---
+++ aula150524/js/script.js
@@ -0,0 +1,4 @@
+var divElement = document.getElementById("principal");
+var pElement = document.createElement('p');
+pElement.innerHTML = "Em cada batida de seu coração sísmico, Godzilla personifica a força imparável da natureza. Seu rugido ecoa como um trovão, anunciando sua chegada iminente, enquanto suas escamas reluzentes refletem a luz como as facetas de uma joia incomparável. Ele é o guardião das profundezas, uma encarnação da magnitude dos oceanos e dos segredos insondáveis das profundezas marinhas.";
+divElement.appendChild(pElement);
